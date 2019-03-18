a0f6e276553f98501f95e94e3548f7fff68ea675
sysbender <sysbender@gmail.com>
2019-03-17 20:33:23 -0400

    abstract data structure

---
+++ src/adt/App.java
@@ -0,0 +1,23 @@
+package adt;
+
+import java.util.LinkedList;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Counter myCounter = new Counter("myCounter");
+		myCounter.increment();
+		myCounter.increment();
+		
+		
+		System.out.println(myCounter);
+		
+		LinkedList list = new LinkedList();
+		
+
+	}
+
+}
---
+++ src/adt/Counter.java
@@ -0,0 +1,24 @@
+package adt;
+
+public class Counter {
+	private String name ;
+	private int value=0;
+	
+	public Counter(String str) {
+		this.name = str;
+	}
+	
+	public void increment() {
+		value++;
+	}
+	
+	
+	public int getCurrentValue() {
+		return value;
+	}
+	
+	public String toString() {
+		return name + " : " + value ;
+	}
+
+}
